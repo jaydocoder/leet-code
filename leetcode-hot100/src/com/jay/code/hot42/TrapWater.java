2e659669316c647e1ed87092c23e170c6c4711f9
yijie <yijie1@yonyou.com>
2025-12-15 14:59:57 +0800

    the second method! adds comments

--- leetcode-hot100/src/com/jay/code/hot42/TrapWater.java
+++ leetcode-hot100/src/com/jay/code/hot42/TrapWater.java
@@ -13,6 +13,7 @@ import java.util.Random;
  * @Version 1.0
  */
 public class TrapWater {
+    //采用动态规划的方法来完成这个题目
     public static int trap(int[] height) {
         int n = height.length;
         if(height==null||n==0)return 0;
@@ -32,6 +33,7 @@ public class TrapWater {
         }
         return ans;
     }
+    //采用双指针来解决这个问题
     public static int trap2(int[] height) {
         int ans = 0;
         int left = 0, right = height.length - 1;
